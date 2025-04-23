ed34a649db8bd9fd899f1aa3fcb255a79333e076
Pushpaganan <pushpaganan@hotmail.com>
2025-04-23 18:22:31 +0530

    Added Image Flip API call for meme generator and useEffect Hook

--- src/components/MemeGenerator/MemeGeneratorBody.jsx
+++ src/components/MemeGenerator/MemeGeneratorBody.jsx
@@ -1,43 +1,67 @@
 import React from 'react'
 
-export default function MemeGeneratorBody(){
-    
-    const [topText,settopText] = React.useState("One does not simply")
-    const [bottomText,setbottomText] = React.useState("Walk into mordor")
+export default function MemeGeneratorBody() {
+    const [count,setCount] = React.useState(0)
+    const [memeImage,setmemeImage] = React.useState([])
 
-    function onTopKeyUpEvent(event){        
-        settopText(event.target.value)
+    React.useEffect(()=>{
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setmemeImage(data.data.memes))
+    },[count])
+
+    const [meme, setMeme] = React.useState(
+        {
+            topText: "One does not simply,",
+            bottomText: "Walk into mordor!",
+            imageUrl: "https://imgflip.com/s/meme/One-Does-Not-Simply.jpg"
+        }
+    )    
+
+    function onChange(event) {
+        const { value, name } = event.currentTarget;
+        setMeme(prev => ({
+            ...prev,
+            [name]: value
+        }))
     }
 
-    function onBottomKeyUpEvent(event){
-        setbottomText(event.target.value)
+    function onGenerateMemeButtonClick() {        
+        setMeme(
+            {
+                ...meme,
+                imageUrl: memeImage[Math.floor(Math.random() * 100)].url
+            }
+        )
     }
 
-    return(
+    return (
         <section className="meme-gen">
             <div className="input-row">
                 <label>Top Text
                     <input
-                    type="text"
-                    placeholder="One does not simply,"
-                    name="topText"
-                    onKeyUp = {onTopKeyUpEvent}
+                        type="text"
+                        placeholder="One does not simply,"
+                        name="topText"
+                        onChange={onChange}
+                        value={meme.topText}
                     />
                 </label>
                 <label>Bottom Text
                     <input
-                    type="text"
-                    placeholder="Walk into mordor"
-                    name="bottomText"
-                    onKeyUp={onBottomKeyUpEvent}
+                        type="text"
+                        placeholder="Walk into mordor"
+                        name="bottomText"
+                        onChange={onChange}
+                        value={meme.bottomText}
                     />
                 </label>
-                <button className="gen-meme-submit">Generate a new Meme</button>
+                <button className="gen-meme-submit" onClick={onGenerateMemeButtonClick}>Generate a new Meme</button>
             </div>
             <div className="image-info">
-                <img src="https://imgflip.com/s/meme/One-Does-Not-Simply.jpg" alt="meme"/>
-                <span className="top">{topText}</span>
-                <span className="bottom">{bottomText}</span>
+                <img src={meme.imageUrl} alt="meme" />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </section>
     )
